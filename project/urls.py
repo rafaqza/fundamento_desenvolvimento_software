9a7989f623cd630c225bef74381a79fa912c7ec9
Rafael Queiroz <rafaqza@gmail.com>
2026-09-09 08:51:23 -0300

    Opção de votar

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
+    path('hello/', include('hello.urls')),  # mantém a rota existente
+    path('forum/', include('forum.urls')),
+    path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'),
 ]
